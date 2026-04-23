adde2a1b5c46c4e72efe487cefc1fc4a8ac5e601
Zhaoyi <zhaoyiz@andrew.cmu.edu>
2026-04-22 20:47:41 -0500

    humaneval llada2.1 eval

--- dllm/pipelines/llada21/__init__.py
+++ dllm/pipelines/llada21/__init__.py
@@ -1,3 +1,12 @@
+from .block_sampler import LLaDA21BlockSampler, LLaDA21BlockSamplerConfig
+from .confidence_block_sampler import (
+    LLaDA21ConfidenceBlockSampler,
+    LLaDA21ConfidenceBlockSamplerConfig,
+)
+from .gibbs_block_sampler import (
+    LLaDA21GibbsBlockSampler,
+    LLaDA21GibbsBlockSamplerConfig,
+)
 from .models.configuration_llada21_moe import LLaDA2MoeConfig
 from .models.modeling_llada21_moe import LLaDA2MoeModelLM
 from .sampler import LLaDA21Sampler, LLaDA21SamplerConfig
@@ -5,6 +14,12 @@ from .sampler import LLaDA21Sampler, LLaDA21SamplerConfig
 __all__ = [
     "LLaDA2MoeConfig",
     "LLaDA2MoeModelLM",
+    "LLaDA21BlockSampler",
+    "LLaDA21BlockSamplerConfig",
+    "LLaDA21ConfidenceBlockSampler",
+    "LLaDA21ConfidenceBlockSamplerConfig",
+    "LLaDA21GibbsBlockSampler",
+    "LLaDA21GibbsBlockSamplerConfig",
     "LLaDA21Sampler",
     "LLaDA21SamplerConfig",
 ]
